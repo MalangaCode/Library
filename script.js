5aa61b0d3cf7a0ddbeffdef8d7fcef9c04d89762
luis <102092058+MalangaCode@users.noreply.github.com>
2023-05-29 17:19:21 -0500

    Create template

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function addBookToLibrary(title, author, pages, read) {
   // do stuff here
   myLibrary.push(new Book(title, author, pages, read))
 }
-addBookToLibrary('padre rico padre pobre', 'robert kiyosaky', 123, 'read');
-addBookToLibrary('Cien anos de soledad', 'Gabriel Garcia Marquez', 200, 'Not read yet');
+// addBookToLibrary('padre rico padre pobre', 'robert kiyosaky', 123, 'read');
+// addBookToLibrary('Cien anos de soledad', 'Gabriel Garcia Marquez', 200, 'Not read yet');
 
 console.log(myLibrary[0].info())
