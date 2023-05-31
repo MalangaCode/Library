51d1e49179a44c4bb1cf2bd9448b7f598c713757
luis <102092058+MalangaCode@users.noreply.github.com>
2023-05-31 17:25:23 -0500

    Add information dialog

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const showButton = document.getElementById('showDialog');
+const favDialog = document.getElementById('favDialog');
+const outputBox = document.querySelector('output');
+const selectEl = favDialog.querySelector('select');
+const confirmBtn = favDialog.querySelector('#confirmBtn');
+
+showButton.addEventListener('click', () => {
+  favDialog.showModal();
+});
+
+selectEl.addEventListener('change', (e) => {
+  confirmBtn.value = selectEl.value;
+});
+
+favDialog.addEventListener('close', (e) => {
+  outputBox.value = favDialog.returnValue === 'default' ? "No return value." : `ReturnValue: ${favDialog.returnValue}.`;
+});
+
+confirmBtn.addEventListener('click', (event) => {
+  event.preventDefault(); // We don't want to submit this fake form
+  favDialog.close(selectEl.value); // Have to send the select box value here.
+});
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
